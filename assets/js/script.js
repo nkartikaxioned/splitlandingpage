ed04c0c05e1a6e194007b7a897d9c798977e4b88
nkartikaxioned <kartikn@axioned.com>
2023-09-30 15:58:08 +0530

    split landing page js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@
 
 */
 
+const sectionFirst=document.querySelector('.first_half');
+const sectionSecond=document.querySelector('.second_half');
+
+window.addEventListener('load',() => {
+
+    sectionFirst.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+    sectionSecond.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+    
+    sectionSecond.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+    sectionFirst.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+})
 
 
 
