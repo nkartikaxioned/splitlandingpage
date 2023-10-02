3cfcb4bf5380157570e11b32f7c8ce69e82521d1
nkartikaxioned <kartikn@axioned.com>
2023-10-02 16:28:00 +0530

    js add

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,24 @@ const wrapperSection=document.querySelector('.wrapper')
 
 window.addEventListener('load',() => {
 
+      sectionFirst.addEventListener('mouseenter', () => {
+                 sectionFirst.style.width='70%';
+                 sectionSecond.style.width='30%';
+      })
+      sectionSecond.addEventListener('mouseenter', () => {
+        sectionSecond.style.width='70%';
+        sectionFirst.style.width='30%';
+})
 
-    left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-    left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+sectionFirst.addEventListener('mouseleave', () => {
+    sectionFirst.style.width='50%';
+    sectionSecond.style.width='50%';
+})
+    // left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+    // left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
     
-    right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-    right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+    // right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+    // right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
    
 })
 
